956fd1eaaca33b7e76458792544d46c2416418e1
Valeev Azamat <azamat@mail.sao.ru>
2024-08-24 21:34:08 +0300

    Настроена работа приложения newsblog

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mainapp.urls', namespace='mainapp'))
+    path('', include('mainapp.urls', namespace='mainapp')),
+    path('blog/', include('newsblog.urls', namespace='newsblog')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.AdminSite.site_header = 'Моя админка'
+admin.AdminSite.index_title = 'Главная страница настройки администрования приложения'
